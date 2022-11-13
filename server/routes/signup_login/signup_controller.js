c911b9dff60af159820e668ff0d0bfcc5578685c
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-11-14 04:32:44 +0530

    signup api with prisma protocol setuped scussesfully jwt token authentication work is next to do

--- server/routes/signup_login/signup_controller.js
+++ server/routes/signup_login/signup_controller.js
@@ -3,23 +3,31 @@ const validator = require("email-validator");
 const bcrypt = require("bcrypt");
 const prisma = require("../../helper/prisma");
 async function signup_controller(req, res) {
-  const { email, password, phone, username } = req.body;
+  const { email, password, phone, username, country_code } = req.body;
+
+  // prisma query
   const user = await prisma.user.findFirst({
     where: {
-      username: username,
+      OR: [
+        {
+          username: username,
+        },
+        {
+          email: email,
+        },
+      ],
     },
   });
-  console.log(user);
-  console.log(req.body);
-  if (email || password || phone || username) {
+  // condition checking
+  if (!email || !password || !phone || !username || !country_code) {
     return res.status(404).json({
       error: "Enter all details",
     });
-  } else if (validator.validate(email)) {
+  } else if (!validator.validate(email)) {
     return res.status(401).json({
       error: "Email enterd is not correct",
     });
-  } else if (phoneLookup(["+91" + phone])[0].name == "") {
+  } else if (phoneLookup([country_code + phone])[0].name == "") {
     return res.status(401).json({
       error:
         "Either you not enter country code or your phone no is entered incorrectly",
@@ -29,19 +37,23 @@ async function signup_controller(req, res) {
       error: "Something is not good",
     });
   } else {
+    /// hashing the password
+
     bcrypt.hash(password, 10, async (err, hash) => {
       if (err) {
         return res.status(500).json({
           error: "Some internal error ocurred we are very sorry",
         });
       }
+
+      // storing the data in data base
       const post = await prisma.user.create({
         data: {
           username: username,
           password: hash,
           bio: "",
           phone: phone,
-          country: phoneLookup(["+91" + phone])[0].country,
+          country: phoneLookup([country_code + phone])[0].name,
           avatar: "",
           email: email,
         },
