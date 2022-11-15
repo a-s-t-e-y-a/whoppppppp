80b98fc21175a7b8fba2a302c1902905adbf85aa
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-11-15 23:48:04 +0530

    xjwt authentication added

--- server/routes/signup_login/login_controller.js
+++ server/routes/signup_login/login_controller.js
@@ -1,6 +1,39 @@
-function login_controller(req, res) {
-  res.json({
-    mes: "login route working properly",
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const prisma = require("../../helper/prisma.js");
+async function login_controller(req, res) {
+  const { username, password } = req.body;
+  console.log(req.body);
+  const user = await prisma.user.findFirst({
+    where: {
+      username: username,
+    },
+  });
+  // check if user exist or not
+  if (!user) {
+    return res.status(401).json({
+      message: "User not found enter valid credentials",
+    });
+  }
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (err) {
+      return res.status(400).json({
+        messgae: "Some error ocuured",
+      });
+    }
+    if (result == true) {
+      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return res.status(200).json({
+        message: "You login scussefully",
+        data: user,
+        token: token,
+      });
+    } else {
+      return res.status(400).json({
+        message: "Enter valid credentials",
+      });
+    }
   });
 }
 module.exports = { login_controller };
