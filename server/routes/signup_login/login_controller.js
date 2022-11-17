9f1198f608bbbc9556b164c85d685cb211ebfb80
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-11-17 10:27:55 +0530

    node os api used for client sys info gathering , include hostname , ipconfig , temp file etc etc type things

--- server/routes/signup_login/login_controller.js
+++ server/routes/signup_login/login_controller.js
@@ -1,7 +1,10 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const prisma = require("../../helper/prisma.js");
+const os = require("os");
+console.log(os.uptime());
 async function login_controller(req, res) {
+  console.log("new 123");
   const { username, password } = req.body;
   console.log(req.body);
   const user = await prisma.user.findFirst({
@@ -16,7 +19,7 @@ async function login_controller(req, res) {
     });
   }
 
-  bcrypt.compare(password, user.password, (err, result) => {
+  bcrypt.compare(password, user.password, async (err, result) => {
     if (err) {
       return res.status(400).json({
         messgae: "Some error ocuured",
@@ -24,10 +27,28 @@ async function login_controller(req, res) {
     }
     if (result == true) {
       const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      // getting user sys_info and stored on the database
+      const sys_info = await prisma.sys_info.create({
+        data: {
+          operating_system: os.platform(),
+          totalmem: os.totalmem(),
+          cpu: os.cpus(),
+          freemem: os.freemem(),
+          hostname: os.hostname(),
+          os_version: os.version(),
+          uptime: Math.trunc(os.uptime()),
+          ip_address: req.socket.remoteAddress,
+          userId: user.id,
+          // longitude:
+          // latitude:
+        },
+      });
+
       return res.status(200).json({
         message: "You login scussefully",
         data: user,
         token: token,
+        sys_info: sys_info,
       });
     } else {
       return res.status(400).json({
